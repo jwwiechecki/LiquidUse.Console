37edd837177463c2130355e0fcfd6aa6dccd13a1
jaroslaw.w.wiechecki <jwwiechecki@gmail.com>
2023-01-28 23:14:33 +0100

    Dodanie klas modelu

---
+++ LiquidUse.Database/Model/LiquidData.cs
@@ -0,0 +1,13 @@
+﻿using LiquidUse.Common.Enums;
+using System;
+
+namespace LiquidUse.Database.Model
+{
+    public class LiquidData
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Use { get; set; }
+        public KindEnum Kind { get; set; }
+    }
+}
