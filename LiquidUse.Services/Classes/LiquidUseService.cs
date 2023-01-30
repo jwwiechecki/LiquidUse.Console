d05477f64957ab64632f1727c3361313a5a3eb9a
jaroslaw.w.wiechecki <jwwiechecki@gmail.com>
2023-01-30 11:55:28 +0100

    Refactor ILiquidUseService.GetItemsByKind(...)

--- LiquidUse.Services/Classes/LiquidUseService.cs
+++ LiquidUse.Services/Classes/LiquidUseService.cs
@@ -39,10 +39,9 @@ namespace LiquidUse.Services.Classes
             DateTime dateFrom = from == null ? DateTime.MinValue : (DateTime)from;
             DateTime dateTo = to == null ? DateTime.MaxValue : (DateTime)to;
 
-            var result = _context.LiquidDatas
+            return _context.LiquidDatas
                 .Where(x => x.Kind == kindEnum && DateTime.Compare(x.Date, dateFrom) >= 0 && DateTime.Compare(x.Date, dateTo) <= 0)
                 .ToList();
-            return result;
         }
 
     }
